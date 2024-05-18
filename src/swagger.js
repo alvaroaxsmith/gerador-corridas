01e5edf61341e508238f87679308ede0a86d7f89
alvaroaxsmith <alvaromachadoferreira@hotmail.com>
2024-05-17 21:39:24 -0300

    fix: add openapi version

--- src/swagger.js
+++ src/swagger.js
@@ -2,21 +2,22 @@ import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
 const swaggerDefinition = {
-  info: {
-    title: 'API de Gerenciamento de Corridas',
-    version: '1.0.0',
-    description: 'Documentação da API de gerenciamento de corridas.',
-  },
-  basePath: '/',
+    openapi: '3.0.0',
+    info: {
+        title: 'API de Gerenciamento de Corridas',
+        version: '1.0.0',
+        description: 'Documentação da API de gerenciamento de corridas.',
+    },
+    basePath: '/',
 };
 
 const options = {
-  swaggerDefinition,
-  apis: ['./src/routes/*.js'],
+    swaggerDefinition,
+    apis: ['./src/routes/rideRoutes.js'],
 };
 
 const swaggerSpec = swaggerJSDoc(options);
 
 export default function setupSwagger(app) {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 }
