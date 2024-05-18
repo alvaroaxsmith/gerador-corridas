1af0ff13b7a4c95ca85eca4cc248d8ee5bb5f8c1
alvaroaxsmith <alvaromachadoferreira@hotmail.com>
2024-05-18 07:40:33 -0300

    feat: add users table

--- src/config.js
+++ src/config.js
@@ -10,15 +10,26 @@ db.serialize(() => {
     created_at TEXT
   )`, (err) => {
     if (err) {
-      console.error('Erro ao criar a tabela:', err.message);
+      console.error('Erro ao criar a tabela de corridas:', err.message);
     } else {
       console.log('Tabela de corridas criada com sucesso.');
+    }
+  });
+
+  db.run(`CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT UNIQUE,
+    password TEXT
+  )`, (err) => {
+    if (err) {
+      console.error('Erro ao criar a tabela de usuários:', err.message);
+    } else {
+      console.log('Tabela de usuários criada com sucesso.');
     }
   });
 });
 
 export const run = (sql, params, callback) => db.run(sql, params, callback);
 export const all = (sql, params, callback) => db.all(sql, params, callback);
-export const update = (sql, params, callback) => db.run(sql, params, callback);
 
 export default db;
