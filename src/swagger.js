695b0cbbe8eeefc03fdfff32fbf7e0a6f8304d4a
alvaroaxsmith <alvaromachadoferreira@hotmail.com>
2024-05-18 07:39:31 -0300

    chore: update swagger config

--- src/swagger.js
+++ src/swagger.js
@@ -2,18 +2,38 @@ import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
 const swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-        title: 'API de Gerenciamento de Corridas',
-        version: '1.0.0',
-        description: 'Documentação da API de gerenciamento de corridas.',
+  openapi: '3.0.0',
+  info: {
+    title: 'API de Corridas',
+    version: '1.0.0',
+    description: 'API para gerenciamento de corridas com autenticação JWT',
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000',
+      description: 'Servidor de desenvolvimento',
     },
-    basePath: '/',
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+    },
+  },
+  security: [
+    {
+      bearerAuth: [],
+    },
+  ],
+  basePath: '/',
 };
 
 const options = {
-    swaggerDefinition,
-    apis: ['./src/routes/rideRoutes.js'],
+  swaggerDefinition,
+  apis: ['./src/routes/rideRoutes.js'],
 };
 
 const swaggerSpec = swaggerJSDoc(options);
