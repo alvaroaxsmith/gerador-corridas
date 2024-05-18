b7b3528a20f8648ef93d6787d63527af73c23881
alvaroaxsmith <alvaromachadoferreira@hotmail.com>
2024-05-18 07:37:46 -0300

    refactor: remove login

--- src/controllers/rideController.js
+++ src/controllers/rideController.js
@@ -1,13 +1,6 @@
 import { createRide, cancelRide, getAllRides } from '../services/rideService.js';
 
 class RideController {
-  /**
-   * Cria uma nova corrida.
-   * 
-   * @param {object} req - O objeto de solicitação HTTP.
-   * @param {object} res - O objeto de resposta HTTP.
-   * @returns {object} - O objeto JSON com os detalhes da corrida criada.
-   */
   static async create(req, res) {
     try {
       const { user_id } = req.body;
@@ -21,13 +14,6 @@ class RideController {
     }
   }
 
-  /**
-   * Cancela uma corrida existente.
-   * 
-   * @param {object} req - O objeto de solicitação HTTP.
-   * @param {object} res - O objeto de resposta HTTP.
-   * @returns {object} - O objeto JSON com os detalhes da corrida cancelada.
-   */
   static async cancel(req, res) {
     try {
       const { id } = req.params;
@@ -38,13 +24,6 @@ class RideController {
     }
   }
 
-  /**
-   * Obtém todas as corridas.
-   * 
-   * @param {object} req - O objeto de solicitação HTTP.
-   * @param {object} res - O objeto de resposta HTTP.
-   * @returns {object} - O objeto JSON com a lista de todas as corridas.
-   */
   static async getAll(req, res) {
     try {
       const rides = await getAllRides();
@@ -54,21 +33,6 @@ class RideController {
       res.status(500).json({ error: err.message });
     }
   }
-
-  static async login(req, res) {
-    try {
-      const { email } = req.body;
-      const userId = await getUserIdByEmail(email);
-      if (!userId) {
-        return res.status(404).json({ error: 'E-mail não encontrado' });
-      }
-      res.status(200).json({ user_id: userId });
-    } catch (err) {
-      console.error('Error during login:', err);
-      res.status(500).json({ error: err.message });
-    }
-  }
-  
 }
 
 export default RideController;
